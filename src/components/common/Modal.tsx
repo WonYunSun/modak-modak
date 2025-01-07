333db1b5094aa5026a5f73f1ab41059f03c0c273
Eunyeong Park <eunyeong.cloud@gmail.com>
2025-01-07 19:43:43 +0900

    chore : 모달 div - flex 속성 추가

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -17,7 +17,7 @@ const Modal: React.FC<ModalProps> = ({ children }) => {
       className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 z-50"
       onClick={closeModal} // 배경 클릭 시 닫기
     >
-      <div className="bg-[#FFF] rounded-[12px] p-6 w-[80%] max-w-md px-5 py-[50px]">
+      <div className="bg-[#FFF] rounded-[12px] p-6 w-[80%] max-w-md px-5 py-[50px] flex flex-col items-center justify-center">
         {children} {/* 내부 요소 렌더링 */}
       </div>
     </div>
