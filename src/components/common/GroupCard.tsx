b73ca8e0a8a9e76c429ce43a616cd26753617236
sanha <heftycornerstone@gmail.com>
2025-01-07 22:16:58 +0900

    feat: GroupCard 컴포넌트에 props 추가, 동적 카드 생성을 위한 준비 마침

--- src/components/common/GroupCard.tsx
+++ src/components/common/GroupCard.tsx
@@ -1,22 +1,28 @@
 import Link from 'next/link';
+import { Database } from '@ts/supabase';
 
-const GroupCard = () => {
+type GroupType = Database['public']['Tables']['groups']['Row'];
+interface GroupCardProps extends GroupType {
+  membersNum: number;
+}
+
+const GroupCard = ({ id, name, description, image_url, membersNum }: GroupCardProps) => {
+  //todo: Link의 href는 모임방 페이지의 경로를 상의한 후 추가하기
+  //todo: 이미지 규격 맞추는 방식 디자이너님과 상의하기
   return (
     <div className="bg-[#FFF] rounded-[12px] shadow-group_card">
       <div className="flex justify-around p-[0.5rem] gap-[0.563rem]">
         <div className="h-[6rem] w-[6rem] bg-[#F2F2F2] rounded-[12px]">
-          <img className="w-full h-full" src={''} alt="group_profile" />
+          <img className="w-full h-full" src={`${image_url}`} alt="group_profile" />
         </div>
         <div className="overflow-hidden flex flex-col justify-center p-[0.25rem 0rem] gap-[0.25rem]">
           <div className="min-w-0 flex-auto">
-            <h4 className="truncate text-lg font-semibold leading-[140%]">{'모닥모닥 그룹 카드'}</h4>
-            <span className="block truncate text-[0.875rem] leading-[140%] text-gray-600">
-              {'한줄 설명이 길어지면 말줄임표가 생길거예요 ABCDEFGHIJKLMN'}
-            </span>
+            <h4 className="truncate text-lg font-semibold leading-[140%]">{name}</h4>
+            <span className="block truncate text-gray-600 text-[0.875rem] leading-[140%]">{description}</span>
           </div>
           <div className="h-[2rem] flex items-center justify-between">
             <div className="flex items-center gap-[2px]">
-              <span className="text-lg leading-[140%]">{'7'}</span>
+              <span className="text-lg leading-[140%]">{membersNum}</span>
               <span className="text-gray-600 text-[0.75rem] leading-[140%]">명 참여 중</span>
             </div>
             <Link href={'/'}>
