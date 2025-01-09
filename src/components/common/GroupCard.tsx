3d5e99d2c941e54185bfa72bf472c5df69d53649
sanha <heftycornerstone@gmail.com>
2025-01-09 21:43:58 +0900

    fix: GroupCard 컴포넌트 레이아웃 조정, 모임방에서 보여야 할 모습 수정

--- src/components/common/GroupCard.tsx
+++ src/components/common/GroupCard.tsx
@@ -14,16 +14,19 @@ interface GroupCardProps {
 const GroupCard = ({ groupInfo, hasLink=true }: GroupCardProps) => {
   const { /*id,*/ name, description, image_url, membersNum } = groupInfo;
   //todo: Link의 href는 모임방 페이지의 경로를 상의한 후 추가하기
+  const titleStyle = `${hasLink && 'truncate'} text-lg font-semibold leading-[140%]`;
+  const descriptionStyle = `${hasLink && 'truncate'} block text-gray-600 text-[0.875rem] leading-[140%]`;
+
   return (
-    <div className="bg-[#FFF] rounded-[0.75rem] shadow-group-card">
-      <div className="flex justify-start p-[0.5rem] gap-[0.563rem]">
-        <div className="h-[6rem] w-[6rem] min-w-[6rem] bg-[#F2F2F2] rounded-[0.75rem] overflow-hidden">
+    <div className="h-[7.938rem] bg-[#FFF] rounded-[0.75rem] shadow-group-card">
+      <div className="h-full flex justify-start items-center p-[0.5rem] gap-[0.563rem]">
+        <div className="h-24 w-24 min-w-24 bg-[#F2F2F2] rounded-[0.75rem] overflow-hidden">
           <img className="w-full h-full" src={`${image_url}`} alt="group_profile" />
         </div>
         <div className="overflow-hidden w-full flex flex-col justify-center p-[0.25rem 0rem] gap-[0.25rem]">
           <div className="min-w-0 flex-auto">
-            <h4 className="truncate text-lg font-semibold leading-[140%]">{name}</h4>
-            <span className="block truncate text-gray-600 text-[0.875rem] leading-[140%]">{description}</span>
+            <h4 className={titleStyle}>{name}</h4>
+            <span className={descriptionStyle}>{description}</span>
           </div>
           <div className="h-[2rem] flex items-center justify-between">
             <div className="flex items-center gap-[0.125rem]">
@@ -32,7 +35,7 @@ const GroupCard = ({ groupInfo, hasLink=true }: GroupCardProps) => {
             </div>
             {hasLink && (
               <Link href={'/'}>
-                <NextArrow/>
+                <NextArrow />
               </Link>
             )}
           </div>
