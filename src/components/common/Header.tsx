77968499cfaa7bf4c5fc2300959408bd1f9c1ae0
sanha <heftycornerstone@gmail.com>
2025-01-09 23:54:34 +0900

    style: 스크롤하면 나의모임 타이틀이 헤더 아래에 붙도록 sticky 적용

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -17,8 +17,8 @@ const Header = ({ home = true, label = '', hasSetting = true }: HeaderProps) => 
   const handleNavigation = () => router.back();
 
   return (
-    <header className="w-full">
-      <div className="w-full fixed px-5 h-12 flex items-center justify-between">
+    <header className="w-full sticky top-0 z-10">
+      <div className="w-full px-5 h-12 flex items-center justify-between">
         {home ? (
           <Image src="" alt="Modak Modak Logo" className="w-20 h-10" />
         ) : (
