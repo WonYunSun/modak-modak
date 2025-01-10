e97e21e085392b0243a1d10e10310afe820b919f
WonYunSun <ereunosun113@gmail.com>
2025-01-10 14:27:49 +0900

    chore: home 컴포넌트 테스트코드 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
-import PostScheduleCard from '@components/common/PostScheduleCard';
-const dummyData = {
-  id: '550e8400-e29b-41d4-a716-446655440000', // uuid 형식
-  name: '3번째 모각코🔥', // text 형식
-  memo: '각자 모여서 공부하고 피드백 하기!', // text 형식
-  start_date: '2025-01-07', // date 형식 (YYYY-MM-DD)
-  end_date: '2025-01-08', // date 형식 (YYYY-MM-DD)
-  start_time: '19:28:00', // time 형식 (HH:MM:SS)
-  created_at: '2024-06-01T12:00:00Z', // timestamptz 형식 (ISO 8601 형식)
-  group_id: '550e8400-e29b-41d4-a716-446655440000'
-};
 const HomePage = () => {
-  return (
-    <div className="inner">
-      <PostScheduleCard {...dummyData} />
-    </div>
-  );
+  return <div>home</div>;
 };
 
 export default HomePage;
