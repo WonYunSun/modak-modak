a6013e9e0937f75230516d43bcbffc98ac4155ae
Eunyeong Park <eunyeong.cloud@gmail.com>
2025-01-10 20:26:46 +0900

    style: tab 버튼 > div로 변경 및 스타일 수정

--- src/app/groups/[id]/_components/Tabs.tsx
+++ src/app/groups/[id]/_components/Tabs.tsx
@@ -8,34 +8,34 @@ const Tabs = () => {
   return (
     <div className="flex">
       {/* 게시글 탭 */}
-      <button
+      <div
         onClick={() => setActiveTab('posts')}
-        className={`flex-1 text-center p-4 pb-3 text-base ${
-          activeTab === 'posts' ? 'border-b-2 border-gray-900 font-bold' : 'text-gray-700 border-b-0'
+        className={`flex-1 text-center text-base ${
+          activeTab === 'posts' ? 'border-b-2 pb-2 border-gray-900 font-semibold' : 'text-gray-700 border-b-0'
         }`}
       >
         게시글
-      </button>
+      </div>
 
       {/* 사진첩 탭 */}
-      <button
+      <div
         onClick={() => setActiveTab('photos')}
-        className={`flex-1 text-center p-4 pb-3 text-base ${
-          activeTab === 'photos' ? 'border-b-2 border-gray-900 font-bold' : 'text-gray-700 border-b-0'
+        className={`flex-1 text-center text-base ${
+          activeTab === 'photos' ? 'border-b-2 pb-2 border-gray-900 font-semibold' : 'text-gray-700 border-b-0'
         }`}
       >
         사진첩
-      </button>
+      </div>
 
       {/* 모임일정 탭 */}
-      <button
+      <div
         onClick={() => setActiveTab('schedules')}
-        className={`flex-1 text-center p-4 pb-3 text-base ${
-          activeTab === 'schedules' ? 'border-b-2 border-gray-900 font-bold' : 'text-gray-700 border-b-0'
+        className={`flex-1 text-center text-base ${
+          activeTab === 'schedules' ? 'border-b-2 pb-2 border-gray-900 font-semibold' : 'text-gray-700 border-b-0'
         }`}
       >
         모임일정
-      </button>
+      </div>
     </div>
   );
 };
