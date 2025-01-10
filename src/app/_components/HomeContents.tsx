70fb34f40b128ee7c6d6226bc8dc8b1105ad4666
sanha <heftycornerstone@gmail.com>
2025-01-10 11:35:55 +0900

    fix: padding-bottom 수정하여 모임카드가 bottom nav에 가리던 문제 해결

--- src/app/_components/HomeContents.tsx
+++ src/app/_components/HomeContents.tsx
@@ -4,7 +4,7 @@ import NoGroup from './NoGroup';
 
 const HomeContents = () => {
   const isGroup = true;
-  const GroupListBoxStyle = `h-auto pb-5 bg-white rounded-t-[1.25rem]`;
+  const GroupListBoxStyle = `h-auto pb-[5.25rem] bg-white rounded-t-[1.25rem]`;
   return (
     <>
       <div className="px-5 pt-[5.625rem] pb-[2.313rem]">
