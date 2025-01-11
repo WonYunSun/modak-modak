b2f98cc30de15905baafcea695ae9c4e14ebcfc3
adorable-otter <plan.stack@gmail.com>
2025-01-11 16:51:43 +0900

    chore: 코드 컨벤션 반영

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -1,5 +1,5 @@
-import { createClient } from '@utils/supabase/server';
 import { NextResponse } from 'next/server';
+import { createClient } from '@utils/supabase/server';
 
 export const GET = async (request: Request) => {
   const supabase = await createClient();
