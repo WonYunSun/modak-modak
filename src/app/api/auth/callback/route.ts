c796e7c5987b089aa377af22411c1dfcf446365f
adorable-otter <plan.stack@gmail.com>
2025-01-11 16:46:26 +0900

    feat: 소셜 로그인 후 세션 생성 기능 추가

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -1,10 +1,14 @@
+import { createClient } from '@utils/supabase/server';
 import { NextResponse } from 'next/server';
 
 export const GET = async (request: Request) => {
+  const supabase = await createClient();
   const { searchParams, origin } = new URL(request.url);
   const error = searchParams.get('error');
-  if (error) {
+  const code = searchParams.get('code');
+  if (error || !code) {
     return NextResponse.redirect(`${origin}/login`);
   }
+  await supabase.auth.exchangeCodeForSession(code);
   return NextResponse.redirect(`${origin}/signup`);
 };
