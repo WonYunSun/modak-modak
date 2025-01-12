9b173d6359e1730f75a89193258cc9c42d69f55d
sanha <heftycornerstone@gmail.com>
2025-01-12 17:55:40 +0900

    style: 멤버 관리창 리더 유저와 일반 유저 다른 UI 적용

--- src/app/groups/[id]/management/members/_components/MemberCard.tsx
+++ src/app/groups/[id]/management/members/_components/MemberCard.tsx
@@ -2,14 +2,47 @@ import { Menu } from '@components/icons';
 import Image from 'next/image';
 import React from 'react';
 
+interface MemberCardBtnsProps {
+  isLeader: boolean;
+  mode: 'curMembers' | 'waiting';
+}
+const MemberCardLeaderBtns = ({ isLeader, mode }: MemberCardBtnsProps) => {
+  return (
+    <>
+      {mode === 'curMembers' ? (
+        isLeader ? (
+          <span className="text-primary">대표</span>
+        ) : (
+          <Menu />
+        )
+      ) : (
+        <div>
+          <button type="button">수락</button>
+          <button type="button" className="ml-4 px-2.5 py-[0.438rem] bg-[#3B82F6] rounded-lg text-white font-semibold">
+            거절
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
+
+const MemberCardStandardBtns = ({ isLeader, mode }: MemberCardBtnsProps) => {
+  return <>{mode === 'curMembers' && isLeader && <span className="text-primary">대표</span>}</>;
+};
+
 interface MemberCardProps {
   isLeader: boolean;
   isMe?: boolean;
-  mode: 'curMembers' | 'waiting'
+  mode: 'curMembers' | 'waiting';
 }
-const MemberCard = ({ isLeader, isMe=false, mode }: MemberCardProps) => {
-    const profile = "https://sozcwgcoibigujehjxbf.supabase.co/storage/v1/object/public/profiles/users/ebcc66fe-bf21-4b73-99d1-e4f375025b80/winterhotchocolate.jpg";
-    
+const MemberCard = ({ isLeader, isMe = false, mode }: MemberCardProps) => {
+  const isLeaderUser = true;
+  
+  const profile =
+    'https://sozcwgcoibigujehjxbf.supabase.co/storage/v1/object/public/profiles/users/ebcc66fe-bf21-4b73-99d1-e4f375025b80/winterhotchocolate.jpg';
+  const name = '이름이에요';
+
   return (
     <div className="w-full border-b border-gray-200">
       <div className="px-5 h-16 flex bg-gray-100 justify-between items-center">
@@ -18,10 +51,14 @@ const MemberCard = ({ isLeader, isMe=false, mode }: MemberCardProps) => {
             <Image src={profile} width={100} height={100} alt={'member_profile'} />
           </div>
           <span className="align-middle">
-            이름이에요 {isMe && <span className="text-gray-500 text-sm">{'(나)'}</span>}
+            {name} {isMe && <span className="text-gray-500 text-sm">{'(나)'}</span>}
           </span>
         </div>
-        {mode==='curMembers' && isLeader ? <span className="text-primary">대표</span> : <Menu />}
+        {isLeaderUser ? (
+          <MemberCardLeaderBtns isLeader={isLeader} mode={mode} />
+        ) : (
+          <MemberCardStandardBtns isLeader={isLeader} mode={mode} />
+        )}
       </div>
     </div>
   );
