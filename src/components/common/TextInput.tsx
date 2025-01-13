6085b953caa1399a933a5371014aae16aa9a4a21
adorable-otter <plan.stack@gmail.com>
2025-01-13 11:58:44 +0900

    refactor: input style이 확장될 수 있게 className 추가

--- src/components/common/TextInput.tsx
+++ src/components/common/TextInput.tsx
@@ -7,9 +7,10 @@ export interface TextInputProps {
   placeHolder?: string;
   value?: string;
   mode?: 'roundBorder' | 'underBar';
+  className?: string;
 }
 
-const TextInput = ({ placeHolder, value, name, onChange, maxLength, mode = 'roundBorder' }: TextInputProps) => {
+const TextInput = ({ placeHolder, value, name, onChange, maxLength, mode = 'roundBorder', className }: TextInputProps) => {
   const modes = {
     roundBorder: 'border border-solid border-gray-300 px-4 py-3 text-base rounded-lg focus:outline-gray-700',
     underBar: 'border-b border-solid border-gray-400 py-2 text-base focus:outline-none'
@@ -18,7 +19,7 @@ const TextInput = ({ placeHolder, value, name, onChange, maxLength, mode = 'roun
   return (
     <div className="flex flex-col gap-2">
       <input
-        className={modes[mode]}
+        className={`${modes[mode]} ${className}`}
         name={name}
         type="text"
         onChange={onChange}
