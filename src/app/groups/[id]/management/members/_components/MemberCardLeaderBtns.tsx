6c8319b294fa7c6decc877de430909e7135b4495
sanha <heftycornerstone@gmail.com>
2025-01-14 20:15:11 +0900

    chore: 클라이언트 컴포넌트 파일 최상단에  use client 추가2

--- src/app/groups/[id]/management/members/_components/MemberCardLeaderBtns.tsx
+++ src/app/groups/[id]/management/members/_components/MemberCardLeaderBtns.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import ManageMembersBtn from './ManageMembersBtn';
 
 interface MemberCardBtnsProps {
--- src/app/groups/[id]/management/members/_components/WaitingMemberList.tsx
+++ src/app/groups/[id]/management/members/_components/WaitingMemberList.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import MemberCard from './MemberCard';
 
 interface WaitingMemberListProps {
