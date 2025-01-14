1a0bdeaac8a729aea8077dcb8fbb82d4fa1665fd
sanha <heftycornerstone@gmail.com>
2025-01-14 14:54:35 +0900

    feat: 모임 탈퇴 모달 UI 추가, +폴더 이름 변경

---
+++ src/app/groups/[id]/management/_components/modal/leaveGroup/LeavingAssure.tsx
@@ -0,0 +1,40 @@
+import useModalStore from '@stores/useModalStore';
+import Button from '@components/common/Button';
+
+interface LeavingAssure {
+  onNextStep:()=>void;
+}
+const LeavingAssure = ({ onNextStep }: LeavingAssure) => {
+  const { closeModal } = useModalStore();
+  const onLeaveGroup = () => {
+    onNextStep();
+  };
+
+  return (
+    <>
+      <div className="w-60 py-6 w-full text-left">
+        <h4 className="mb-5 text-lg font-semibold text-gray-900">모임을 정말 탈퇴하시겠어요?</h4>
+        <div className="w-full">
+          모임을 탈퇴해도 <span className="font-semibold">작성한 글과</span>
+          <br />
+          <span className="font-semibold">댓글은 자동으로 삭제되지 않아요</span>
+        </div>
+      </div>
+      <div className="w-full mt-[1.125rem] flex gap-2 justify-center">
+        <div className="w-[4.375rem]">
+          <Button type={'button'} className={'modal-white-btn'} label={'취소'} onClick={closeModal} />
+        </div>
+        <div className="w-44">
+          <Button
+            type={'button'}
+            className={'modal-full-btn'}
+            label={'탈퇴하기'}
+            onClick={onLeaveGroup}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LeavingAssure;
---
+++ src/app/groups/[id]/management/_components/modal/leaveGroup/LeavingSuccess.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import Button from "@components/common/Button";
+
+const LeavingSuccess = () => {
+  return (
+    <>
+      <div className="w-full py-6 text-left">
+        <h4 className="mb-1 text-lg font-semibold text-gray-900">모임이 삭제되었어요</h4>
+        <div className="text-gray-500">다시 뵐 수 있으면 좋겠어요!</div>
+      </div>
+      <Link href="/" className="w-full mt-5">
+        <Button type={'button'} className={'full-btn'} label={'확인'} />
+      </Link>
+    </>
+  );
+}
+
+export default LeavingSuccess
