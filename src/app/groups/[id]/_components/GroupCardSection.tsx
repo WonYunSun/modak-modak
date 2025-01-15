ee2c10b9c5d92dc1155a102161d2f2234a1494e7
Eunyeong Park <eunyeong.cloud@gmail.com>
2025-01-15 21:51:12 +0900

    feat: 그룹 정보 불러오기 추가

--- src/app/groups/[id]/_components/GroupCardSection.tsx
+++ src/app/groups/[id]/_components/GroupCardSection.tsx
@@ -4,24 +4,23 @@ import { useParams } from 'next/navigation';
 
 import GroupCard from '@components/common/groupCard/GroupCard';
 
+import { useFetchGetGroupInfo } from '@hooks/group/useFetchGroup';
+
 export const GroupCardSection = () => {
   const { id } = useParams();
   const groupId = Array.isArray(id) ? id[0] : id;
 
-  // TODO: 그룹 정보 가져오는 쿼리 실행 후 data 전달하기
+  const { data, isPending, isError } = useFetchGetGroupInfo(groupId);
+
+  if (isPending) return <p>로딩 중...</p>;
+
+  if (isError || !data) {
+    <span>모임 정보를 불러오는데 문제가 발생했습니다.</span>;
+  }
+
   return (
     <div className="w-full px-5 -mt-[2.75rem] z-10">
-      <GroupCard
-        groupInfo={{
-          id: groupId,
-          name: '모각코!',
-          description: '안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요',
-          image_url: '/icons/group-image.webp',
-          membersNum: 7,
-          created_at: '2025-01-14',
-        }}
-        hasLink={false}
-      />
+      <GroupCard groupInfo={data!} hasLink={false} />
     </div>
   );
 };
