f408ed65b2955db9fa278a8424e321447d470e6c
Eunyeong Park <eunyeong.cloud@gmail.com>
2025-01-15 19:27:21 +0900

    fix: Tab 작동 방식 수정, useTabStore 삭제

--- src/app/groups/[id]/_components/TabPages.tsx
+++ src/app/groups/[id]/_components/TabPages.tsx
@@ -1,18 +1,17 @@
 'use client';
 
-import { useTabStore } from 'stores/useTabStore';
-
 import Tabs from './Tabs';
 import PostList from './PostList';
 import PhotoList from './PhotoList';
 import ScheduleList from './ScheduleList';
+import { useState } from 'react';
 
 const TabPages = () => {
-  const { activeTab } = useTabStore();
+  const [activeTab, setActiveTab] = useState('posts');
 
   return (
     <div className="w-full mx-auto px-5 mt-8">
-      <Tabs />
+      <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
       <div className="">
         {activeTab === 'posts' && <PostList />}
         {activeTab === 'photos' && <PhotoList />}
