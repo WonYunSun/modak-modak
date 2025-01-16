82c0148cf2df2c37570451d6026bc0ea2211f438
sanha <heftycornerstone@gmail.com>
2025-01-16 23:55:55 +0900

    feat: 몇몇 모달에서 마지막 스텝에서 바깥을 클릭해도 홈 화면으로 이동하도록 함

--- src/app/groups/[id]/management/_components/modal/ManagementModal.tsx
+++ src/app/groups/[id]/management/_components/modal/ManagementModal.tsx
@@ -1,30 +1,37 @@
 'use client';
 
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import Modal from '@components/common/Modal';
-import { ModalModeType } from '@app/groups/[id]/management/_components/ManagementContents';
 import ChangeGroupProfileModal from '@app/groups/[id]/management/_components/modal/changeGroupProfile/ChangeGroupProfileModal';
 import DeleteGroupModal from '@app/groups/[id]/management/_components/modal/deleteGroup/DeleteGroupModal';
 import LeaveGroupModal from '@app/groups/[id]/management/_components/modal/leaveGroup/LeaveGroupModal';
 import LeaderTransitionModal from '@app/groups/[id]/management/_components/modal/leaderTransition/LeaderTransitionModal';
-import { useState } from 'react';
+import { ModalModeType } from '@app/groups/[id]/management/_components/ManagementContents';
+
+const LEAVEONLASTSTEP = ['deleteGroup', 'leaveGroup'];
 
 interface ManagementModalProps {
   isLeader: boolean;
   modalMode: ModalModeType | null;
 }
 const ManagementModal = ({ isLeader, modalMode }: ManagementModalProps) => {
+  const router = useRouter();
   const [isLastStep, setIsLastStep] = useState(false);
+  const lastStepSetter = () => {
+    setIsLastStep(true);
+  };
 
   const modals = {
     changeProfile: <ChangeGroupProfileModal />,
-    deleteGroup: <DeleteGroupModal />,
-    leaveGroup: <LeaveGroupModal isLeader={isLeader} />,
+    deleteGroup: <DeleteGroupModal lastStepSetter={lastStepSetter} />,
+    leaveGroup: <LeaveGroupModal isLeader={isLeader} lastStepSetter={lastStepSetter} />,
     leaderTransition: <LeaderTransitionModal />,
   };
-  const leaveOnLastStep = ['deleteGroup', 'leaveGroup'];
 
   const onClickOutSide = () => {
-    if (leaveOnLastStep.find((mode) => mode === modalMode) && isLastStep) {
+    if (LEAVEONLASTSTEP.find((mode) => mode === modalMode) && isLastStep) {
+      router.push('/');
     }
   };
 
