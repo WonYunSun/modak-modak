692af0e42ee252246d36786a3e729a744d6dcc18
WonYunSun <ereunosun113@gmail.com>
2025-01-17 20:59:26 +0900

    feat: 모임 스케쥴 카드 클릭 시 일정 상세로 라우팅

--- src/components/common/scheduleCard/ScheduleCardList.tsx
+++ src/components/common/scheduleCard/ScheduleCardList.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import ScheduleCard from './ScheduleCard';
 import SearchBar from '@app/groups/[id]/_components/SearchBar';
 import CountBar from '@app/groups/[id]/_components/CountBar';
@@ -20,7 +20,7 @@ const fetchScheduleDatas = async (groupId: string) => {
 const ScheduleCardList = () => {
   const { id } = useParams();
   const groupId = Array.isArray(id) ? id[0] : id;
-
+  const router = useRouter();
   const [scheduleData, setScheduleData] = useState<ScheduleType[]>([]);
 
   useEffect(() => {
@@ -34,13 +34,16 @@ const ScheduleCardList = () => {
     fetchData();
   }, []);
 
+  const goToScheduleDetail = (scheduleId: string) => {
+    router.push(`/groups/${groupId}/schedules/${scheduleId}`);
+  };
   return (
     <div>
       <SearchBar />
       <CountBar value={scheduleData.length} />
       <div className="mt-4 mb-20">
         {scheduleData.map((schedule, index) => (
-          <div className="mb-5" key={index}>
+          <div className="mb-5" key={index} onClick={() => goToScheduleDetail(schedule.id)}>
             <ScheduleCard
               name={schedule.name}
               memo={schedule.memo}
