7d5571461648656ab4c3b0e8e241e10c986c1d63
Eunyeong Park <eunyeong.cloud@gmail.com>
2025-01-18 00:46:23 +0900

    style:

--- src/components/common/Spinner.tsx
+++ src/components/common/Spinner.tsx
@@ -1,6 +1,6 @@
 const Spinner = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-white/60">
+    <div className="fixed inset-0 flex items-center justify-center bg-white/50">
       <div
         className="w-8 h-8 border-2 rounded-full animate-spin border-t-[#B94600] border-l-[#FFF2EB] border-r-[#FFF2EB] border-b-transparent"
         role="status"
