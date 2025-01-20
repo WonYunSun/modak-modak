92a3ce4c2d26d3f3ad4883d09120cddc3dc2d918
adorable-otter <plan.stack@gmail.com>
2025-01-20 12:16:49 +0900

    fix: 병합 오류 수정

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -5,23 +5,8 @@ import { NextResponse, type NextRequest } from 'next/server';
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
-    request,
   });
 
-  const supabase = createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!, {
-    cookies: {
-      getAll() {
-        return request.cookies.getAll();
-      },
-      setAll(cookiesToSet) {
-        cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
-        supabaseResponse = NextResponse.next({
-          request,
-        });
-        cookiesToSet.forEach(({ name, value, options }) => supabaseResponse.cookies.set(name, value, options));
-      },
-    },
-  });
   const supabase = createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!, {
     cookies: {
       getAll() {
@@ -40,7 +25,6 @@ export async function updateSession(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
   if (!isPublicRoute(pathname)) {
     const {
-      data: { user },
       data: { user },
     } = await supabase.auth.getUser();
 
