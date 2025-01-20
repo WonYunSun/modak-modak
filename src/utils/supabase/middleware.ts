67e721474c4a7a1879ed5e00fe871877ee5272a2
adorable-otter <plan.stack@gmail.com>
2025-01-20 12:13:11 +0900

    feat: signup 관련 분기 처리 변경

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -28,6 +28,12 @@ export async function updateSession(request: NextRequest) {
       data: { user },
     } = await supabase.auth.getUser();
 
+    if (user && !user.user_metadata.nickname && !pathname.startsWith('/signup')) {
+      const url = request.nextUrl.clone();
+      url.pathname = '/signup';
+      return NextResponse.redirect(url);
+    }
+
     // 인증이 필요한 페이지인데 로그인하지 않은 경우 로그인 페이지로 이동
     if (!isAuthenticated(user) && needsAuthentication(pathname)) {
       const url = request.nextUrl.clone();
