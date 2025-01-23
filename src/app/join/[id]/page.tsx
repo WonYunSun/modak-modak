0ffc52142219c5ff1c9704691512e3d144e8ce5e
adorable-otter <plan.stack@gmail.com>
2025-01-23 14:23:52 +0900

    refactor: 로고 화질을 위해 svg로 변경 및 가입 페이지에서 home으로 이동할 수 있는 기능 추가

--- src/app/join/[id]/page.tsx
+++ src/app/join/[id]/page.tsx
@@ -1,12 +1,15 @@
-import Image from 'next/image';
+import Link from 'next/link';
 import JoinContents from '@app/join/[id]/_components/JoinContents';
+import { Logo } from '@components/icons';
 
 const Join = () => {
   return (
     <>
       <header className={`w-full fixed top-0 left-0 right-0 z-10`}>
         <div className="px-5 h-12 flex items-center justify-start">
-          <Image src="/icons/modakLogo.webp" width={80} height={34} alt="Modak Modak Logo" className="w-20 h-10" />
+          <Link href={'/'}>
+            <Logo className="w-20 h-10" />
+          </Link>
         </div>
       </header>
       <main className="h-screen w-screen">
