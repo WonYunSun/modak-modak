48eaaf0c91c6c9a6ec42991831731c009b36cccb
sanha <heftycornerstone@gmail.com>
2025-02-03 11:37:32 +0900

    fix: 모임 삭제 이후 다른 모임의 관리페이지로 들어가면 모달이 뜨는 버그 고침 +몇몇 페이지 스타일링 수정

--- src/app/notifications/page.tsx
+++ src/app/notifications/page.tsx
@@ -3,11 +3,11 @@ import FunnelHeader from '@components/common/FunnelHeader';
 
 const Notification = () => {
   return (
-    <div className="mx-auto max-w-[600px] h-screen overflow-y-scroll scrollbar-hide">
-      <div className="sticky w-[600px] left-0 top-0 bg-white">
+    <div className="mx-auto max-w-[600px] h-screen overflow-y-scroll scrollbar-hide border-x border-gray-200">
+      <div className="sticky w-[600px] left-0 top-0 bg-white px-5">
         <FunnelHeader label="알림" />
       </div>
-      <NotificationsContents/>
+      <NotificationsContents />
     </div>
   );
 };
