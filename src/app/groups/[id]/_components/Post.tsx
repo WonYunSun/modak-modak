501681b83f676a8716066541044de6d717cf19b4
Eunyeong Park <eunyeong.cloud@gmail.com>
2025-02-03 12:26:59 +0900

    chore: 사용하지 않는 import 수정

--- src/app/groups/[id]/_components/Post.tsx
+++ src/app/groups/[id]/_components/Post.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Image from 'next/image';
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 import { Comments, Menu } from '@components/icons';
 
