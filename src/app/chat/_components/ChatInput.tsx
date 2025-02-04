88a1c24d3c4c4c034d08b6ea1f94037b4c7e52a0
kminhoo <whdgus4158@gmail.com>
2025-02-04 17:19:43 +0900

    style: style 일부 수정

--- src/app/chat/_components/ChatInput.tsx
+++ src/app/chat/_components/ChatInput.tsx
@@ -51,7 +51,7 @@ const ChatInput = ({ user, chatRoomId }: ChatInputProps) => {
   };
 
   return (
-    <div className="px-5 py-2 absolute bottom-0 left-0 right-0 z-10">
+    <div className="px-5 py-2 absolute bottom-0 left-0 right-0 z-10 bg-white">
       <div className="w-full border bg-white px-3 py-2 rounded-lg flex items-center gap-1">
         <textarea
           rows={1}
