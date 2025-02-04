94792dcb12736e539625dd91bb6f4f95b921bf52
adorable-otter <plan.stack@gmail.com>
2025-02-04 12:33:55 +0900

    feat: sentry 설정 업데이트

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: 'https://67472d6d03d07769f77991c60e22fa21@o4508685086294016.ingest.us.sentry.io/4508685090947072',
+  dsn: 'https://3259a3fddb87ba6c27b061dd03477326@o4508758711468032.ingest.us.sentry.io/4508758722215936',
 
   // Add optional integrations for additional features
   integrations: [Sentry.replayIntegration()],
