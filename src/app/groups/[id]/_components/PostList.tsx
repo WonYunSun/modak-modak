41f874594a52c203539a70603935fe30bcc3ec03
Eunyeong Park <eunyeong.cloud@gmail.com>
2025-02-05 20:47:34 +0900

    fix: 페이지네이션 마지막 데이터 누락되는 부분 수정

--- src/app/groups/[id]/_components/PostList.tsx
+++ src/app/groups/[id]/_components/PostList.tsx
@@ -34,6 +34,7 @@ const PostList = () => {
 
   const posts = data?.pages.flat() || [];
 
+  console.log('posts', posts);
   useEffect(() => {
     if (!loadPostRef.current || !hasNextPage) return;
     const observer = new IntersectionObserver(
--- src/hooks/post/useFetchGetPosts.ts
+++ src/hooks/post/useFetchGetPosts.ts
@@ -9,8 +9,10 @@ export const useFetchGetPosts = (groupId: string, searchTerm?: string) => {
     queryFn: ({ pageParam = 0 }) => getPosts(groupId, Number(pageParam), PAGE_SIZE, searchTerm),
     initialPageParam: 0,
     getNextPageParam: (lastPage, allPages) => {
-      // 데이터가 비어 있으면 페이지네이션 종료
-      return lastPage.length > 0 ? allPages.length + 1 : undefined;
+      if (!lastPage || lastPage.length === 0) {
+        return undefined; // 페이지네이션 종료
+      }
+      return allPages.length; // 다음 페이지 번호 반환
     },
   });
 
