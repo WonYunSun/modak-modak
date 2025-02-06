69d9cf08e57870c782204e43741ae041efc57b86
Eunyeong Park <eunyeong.cloud@gmail.com>
2025-02-07 00:43:56 +0900

    fix: 멤버 페이지 헤더 수정

--- src/app/groups/[id]/management/members/page.tsx
+++ src/app/groups/[id]/management/members/page.tsx
@@ -1,11 +1,11 @@
-import Header from '@components/common/Header';
+import FunnelHeader from '@components/common/FunnelHeader';
 import MembersPageContents from '@app/groups/[id]/management/members/_components/MembersPageContents';
 
 const MembersPage = () => {
   return (
     <div className="mx-auto max-w-[600px] h-[100dvh] overflow-y-scroll scrollbar-hide">
       <div className="h-full border-x border-gray-200">
-        <Header home={false} label={'멤버 목록'} isScrolled={true} />
+        <FunnelHeader label={'멤버 목록'} />
         <div className="pt-6 mb-36 scrollbar-hide">
           <MembersPageContents />
         </div>
